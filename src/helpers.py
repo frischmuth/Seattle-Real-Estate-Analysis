63f6ba8b0b6cc35766eaf3e46a1973849a2617ed
Ross Frischmuth <frischmuth@gmail.com>
2019-11-05 15:25:04 -0800

    begin spark functionality

--- src/helpers.py
+++ src/helpers.py
@@ -1,7 +1,51 @@
 import numpy as np
 import pandas as pd
 
-def get_pending_demo_permits(filepath):
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, OneHotEncoderEstimator, VectorAssembler
+
+def gis_data_to_spark(filepath='data/Parcels_for_King_County_with_Address_with_Property_Information__parcel_address_area.csv'):
+    spark = SparkSession\
+    .builder\
+    .master('Local[4]')\
+    .appName("GetGISData")\
+    .getOrCreate()
+    
+    gis = get_gis_data()
+    gis_spark = spark.createDataFrame(gis)
+    
+    cat_cols = ['SITETYPE', 'LEVYCODE', 'NEW_CONSTR', 'TAXVAL_RSN', 'QTS', 'SEC', 'TWP', 'RNG', 'PROPTYPE', 'KCA_ZONING', 'PREUSE_DESC']
+    dummies = []
+    for col in cat_cols:
+        dummies.append(col+'vector')
+    
+    # Transforms all strings into indexed numbers
+    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(df) for column in cat_cols]
+    pipeline = Pipeline(stages=indexers)
+    df = pipeline.fit(df).transform(df)
+
+    encoder = OneHotEncoderEstimator(inputCols=cat_cols,outputCols=dummies,extr)
+    model = encoder.fit(gis_spark)
+    gis_spark = model.transform(gis_spark)   
+    
+    
+    
+    
+    
+    gis_spark.write.parquet('data/gis_parquet',mode='overwrite')
+    return gis_spark
+
+
+
+
+
+
+
+
+
+
+def get_pending_demo_permits(filepath='data/Building_permits.csv'):
     building_permits = pd.read_csv(filepath ,parse_dates=[10,11,12,13], infer_datetime_format=True)
     res_bldg_prmts = building_permits[building_permits['PermitClassMapped']=='Residential'].copy()
     demo = res_bldg_prmts[res_bldg_prmts['PermitTypeDesc']=='Demolition']
@@ -12,7 +56,7 @@ def get_pending_demo_permits(filepath):
     return pending_demo
 
 def get_gis_data(filepath='data/Parcels_for_King_County_with_Address_with_Property_Information__parcel_address_area.csv'):
-    gis = pd.read_csv(filepath, index_col='PIN', low_memory=False)
+    gis = pd.read_csv(filepath, low_memory=False)
     
     # Limit to Seattle Residential
     gis = gis[gis['CTYNAME']=='SEATTLE']
@@ -24,17 +68,17 @@ def get_gis_data(filepath='data/Parcels_for_King_County_with_Address_with_Proper
     gis = gis[gis['KCTP_STATE'].notnull()]
     
     # drop ACCNT_NUMBER - cannot tie to Seattle data
-    gis = gis[['MAJOR', 'MINOR',  'SITETYPE', 'ADDR_FULL', 'LAT', 'LON', 
+    gis = gis[['PIN', 'MAJOR', 'MINOR',  'SITETYPE', 'ADDR_FULL', 'LAT', 'LON', 
                 'KCTP_STATE', 'LOTSQFT', 'LEVYCODE', 'NEW_CONSTR', 'TAXVAL_RSN', 'APPRLNDVAL', 
                 'APPR_IMPR', 'TAX_LNDVAL', 'TAX_IMPR', 'QTS', 'SEC', 'TWP', 'RNG', 
                 'Shape_Length', 'Shape_Area', 'PROPTYPE', 'KCA_ZONING', 
                 'KCA_ACRES', 'PREUSE_DESC']]
 
     # Create Dummy Columns
-    dummy_cols = ['SITETYPE', 'LEVYCODE', 'NEW_CONSTR', 'TAXVAL_RSN', 'QTS', 'SEC', 'TWP', 'RNG', 
-                'PROPTYPE', 'KCA_ZONING', 'PREUSE_DESC']
-    for col in dummy_cols:
-        gis = pd.concat([gis,pd.get_dummies(gis[col], prefix=col,dummy_na=True)],axis=1).drop([col],axis=1)
+    # dummy_cols = ['SITETYPE', 'LEVYCODE', 'NEW_CONSTR', 'TAXVAL_RSN', 'QTS', 'SEC', 'TWP', 'RNG', 
+    #             'PROPTYPE', 'KCA_ZONING', 'PREUSE_DESC']
+    # for col in dummy_cols:
+    #     gis = pd.concat([gis,pd.get_dummies(gis[col], prefix=col,dummy_na=True)],axis=1).drop([col],axis=1)
     return gis
     
 
@@ -52,17 +96,16 @@ def get_parcels(filepath='data/EXTR_Parcel.csv'):
        'SubArea','DistrictName', 'LevyCode',
        'CurrentZoning', 'PresentUse',
        'SqFtLot', 'WaterSystem', 'SewerSystem', 'Access', 'Topography',
-       'StreetSurface', 'InadequateParking',
-       'PcntUnusable', 'Unbuildable', 'MtRainier', 'Olympics', 'Cascades',
+       'StreetSurface', 'InadequateParking', 
+       'Unbuildable', 'MtRainier', 'Olympics', 'Cascades',
        'Territorial', 'SeattleSkyline', 'PugetSound', 'LakeWashington',
        'LakeSammamish', 'SmallLakeRiverCreek', 'OtherView', 'WfntLocation',
-       'WfntFootage', 'WfntBank', 'WfntPoorQuality', 'WfntRestrictedAccess',
-       'WfntAccessRights', 'WfntProximityInfluence', 'TidelandShoreland',
-       'LotDepthFactor', 'TrafficNoise', 'AirportNoise', 'PowerLines',
-       'OtherNuisances', 'NbrBldgSites', 'Contamination']
+        'WfntBank', 'WfntPoorQuality', 'WfntRestrictedAccess',
+       'WfntAccessRights',  'TidelandShoreland',
+       'LotDepthFactor', 'TrafficNoise', 'NbrBldgSites', 'Contamination']
     
-    for col in dummy_cols:
-        parcels = pd.concat([parcels,pd.get_dummies(parcels[col], prefix=col,dummy_na=True)],axis=1).drop([col],axis=1)
+    # for col in dummy_cols:
+    #     parcels = pd.concat([parcels,pd.get_dummies(parcels[col], prefix=col,dummy_na=True)],axis=1).drop([col],axis=1)
     
     # Create Binary Columns
     binary_cols = ['AdjacentGolfFairway', 'AdjacentGreenbelt', 'HistoricSite',
@@ -72,9 +115,10 @@ def get_parcels(filepath='data/EXTR_Parcel.csv'):
         'HundredYrFloodPlain', 'SeismicHazard', 'LandslideHazard',
         'SteepSlopeHazard', 'Stream', 'Wetland', 'SpeciesOfConcern',
         'SensitiveAreaTract', 'WaterProblems', 'TranspConcurrency',
-        'OtherProblems']
-    for col in binary_cols:
-        parcels[col] = parcels[col].map(lambda x: True if x=='Y' else False)
+        'OtherProblems''WfntProximityInfluence','PowerLines', 
+       'OtherNuisances']
+    # for col in binary_cols:
+        # parcels[col] = parcels[col].map(lambda x: True if x=='Y' else False)
     
     return parcels
 
